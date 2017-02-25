c150ccafa9e3437388f7e4cf7f615b9662db19ce
MichaelTonyFerrari <ferraro2ndbase@gmail.com>
2017-02-25 15:09:36 -0500

    Updated pointers

--- pointer.c
+++ pointer.c
@@ -1,9 +1,27 @@
-#include <iostream.h>
+#include <stdio.h>
 #include <string.h>
+
+// Pointers allow us to point to a memory address 
 
 int main(){
 
+	char str[20];
+	char *one;
+	char *two; 
 
+	// Copy hello everyone into str 
+	strcpy(str, "Hello Everyone\n");
+	
+	// Set first pointr to start of str array 
+	one = str;
+	printf(one);
+
+	// Set the second one 2 bytes further in the array 	
+	two = one + 2; 
+	printf(two);
+	
+	strcpy(two, "y you guys!\n");
+	printf(one);
 
 }
 
